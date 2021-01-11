84d6698c6b3064efed8f8398504d6de52e12dcaf
rcai7774 <rcai7774@gmail.com>
2021-01-11 03:18:46 +0000

    stuffs

---
+++ index.js
@@ -0,0 +1,45 @@
+// index.js
+
+const express = require("express");
+const server = express();
+
+const body_parser = require("body-parser");
+
+// parse JSON (application/json content-type)
+server.use(body_parser.json());
+
+const port = 4000;
+
+// << db setup >>
+const db = require("./db");
+const dbName = "data";
+const collectionName = "movies";
+
+// << db init >>
+db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+    // get all items
+    dbCollection.find().toArray(function(err, result) {
+        if (err) throw err;
+          console.log(result);
+    });
+
+    // << db CRUD routes >>
+server.post("/items", (request, response) => {
+    const item = request.body;
+    dbCollection.insertOne(item, (error, result) => { // callback of insertOne
+        if (error) throw error;
+        // return updated list
+        dbCollection.find().toArray((_error, _result) => { // callback of find
+            if (_error) throw _error;
+            response.json(_result);
+        });
+    });
+});
+
+}, function(err) { // failureCallback
+    throw (err);
+});
+
+server.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
